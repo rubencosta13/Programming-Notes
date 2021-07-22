cec23779204d53eacf6fc9edd5be9222b132d977
rubencosta13 <66113929+rubencosta13@users.noreply.github.com>
2021-07-22 20:15:08 +0100

    Added folders as well as the first C explanation example for printf and scanf

---
+++ Programming Languages/C/#1 - Libraries and Prints.c
@@ -0,0 +1,5 @@
+#include <stdio.h> // Standard Input and Output Library (For Mouse / Keyboard (input devices) as well as monitors and printers(output devices))
+
+int main(){
+    printf("Hello World");          // This line of code will print on the VSC terminal "Hello World"
+}
